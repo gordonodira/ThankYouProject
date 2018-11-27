dda8de4e7ae3b08fb2c5453bba137071eb4dad70
Gordon Odira <sellemap@gmail.com>
2018-11-27 14:42:07 -0500

    Fixed Home page and jsfooter file

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,8 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-
+<div class="col-md-6">
     <h1>This is my About Page</h1>
-    <p class="lead">Use this document as a way to quickly start any new project.<br>All you get is this text and a mostly barebones HTML document.</p>
+    <p class="lead">Use this document as a way to quickly start any new project.<br>
+        All you get is this text and a mostly barebones HTML document.</p>
 
+</div>
 @endsection;
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@ Route::get('/about', 'pagesController@about')->name('about');
 Route::get('/contact', 'pagesController@contact')->name('contact');
 Route::post('/contact', 'pagesController@store')->name('contact.Store');
 Route::get('/thanks/{name}', 'pagesController@thanks')->name('thanks');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
